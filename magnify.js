a9a12208bb9a5bf3780a8b6e3a75e6cd0912e9c9
sara_h_chen <deutscher71112@gmail.com>
2016-03-19 21:15:33 +0000

    Changed all code

---
+++ magnify.js
@@ -0,0 +1,13 @@
+<script type="text/javascript" src="Event.js"></script>
+<script type="text/javascript" src="Magnifier.js"></script>
+<script type="text/javascript">
+var evt = new Event(),
+	m = new Magnifier(evt, {
+		zoom: 3,
+		zoomable: true
+});
+</script>
+
+m.attach({
+	thumb
+});
